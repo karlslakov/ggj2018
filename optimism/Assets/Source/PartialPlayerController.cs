0c5ffbccfa419ada6bd8e46180fa81ba6dc46416
karlslakov <karlslakov@gmail.com>
2018-01-27 10:37:04 -0800

    basic health

--- optimism/Assets/Source/PartialPlayerController.cs
+++ optimism/Assets/Source/PartialPlayerController.cs
@@ -8,38 +8,20 @@ public class PartialPlayerController : PunBehaviour {
 		
 	}
 	
-	/*
-	void Update () {
-        float speed = 1f;
-        
-        if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-
-        if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-
-        if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-
-        if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-    }
-    */
+	
     void Update () {
         float speed = 1f;
         Vector3 move = Vector3.zero;
+
         if (Input.GetKey(KeyCode.D))
             move += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-
         if (Input.GetKey(KeyCode.A))
             move -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
-
         if (Input.GetKey(KeyCode.W))
             move += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-
         if (Input.GetKey(KeyCode.S))
             move -= new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
-
+        
         photonView.RPC("Move", PhotonTargets.All, move);
     }
 
---
+++ optimism/Assets/Source/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using Photon;
+using UnityEngine;
+
+public class PlayerHealth : PunBehaviour {
+
+    RectTransform HealthBar;
+    float healthPct = 100;
+    Vector2 baseSizeDelta;
+
+    void Start () {
+        HealthBar = GameObject.Find("HealthFull").GetComponent<RectTransform>();
+        baseSizeDelta = HealthBar.sizeDelta;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown("k"))
+            photonView.RPC("Dmg", PhotonTargets.AllBufferedViaServer, 10f);
+	}
+
+    [PunRPC]
+    public void Dmg(float amount)
+    {
+        healthPct -= amount;
+        HealthBar.sizeDelta = new Vector2(baseSizeDelta.x * healthPct / 100f, baseSizeDelta.y);
+    }
+
+}
