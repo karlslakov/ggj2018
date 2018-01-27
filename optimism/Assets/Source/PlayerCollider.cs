1afa081d0ba3e9e611a174ce97d0ebdc067fa619
qwartzclock <ontograsstown@gmail.com>
2018-01-27 12:34:03 -0800

    Organized folders and added collision functionality

---
+++ optimism/Assets/Source/PlayerCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon;
+using UnityEngine;
+
+public class PlayerCollider : PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		Debug.Log ("collide! "+coll.gameObject.name);
+		if (coll.gameObject.tag == "Hazard") {
+			
+		}
+	}
+}
